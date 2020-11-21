ff140e6dbafab3f6c563e6c419d20e45c123434a
OveR1st <315overdose@gmail.com>
2020-11-21 14:12:58 +0200

    4.7 удаление элемента списка через setState НЕ ИЗМЕНЯЯ ГЛАВНЫЙ МАССИВ

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
 import AppHeader from '../app-header';
@@ -8,26 +8,70 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+export default class App extends Component {
+
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+    ]
+  }
+
+  deleteItem = (id) => {
+    this.setState( ( {todoData} ) => {
+      const idx = todoData.findIndex( (el) => el.id === id );
+      
+
+      // [a, b, c, d, e]
+      // [a, b,    d, e]
+      
+
+      const newArray = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+
+      return {
+        todoData: newArray
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
   
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+        <TodoList
+          todos={this.state.todoData}
+          onDeleted={ this.deleteItem } />
       </div>
+    );
+  };
+} 
 
-      <TodoList todos={todoData}
-      onDeleted={ (id) => console.log('del', id)} />
-    </div>
-  );
-};
+// const App = () => {
 
-export default App;
+//   const todoData = [
+//     { label: 'Drink Coffee', important: false, id: 1 },
+//     { label: 'Make Awesome App', important: true, id: 2 },
+//     { label: 'Have a lunch', important: false, id: 3 }
+//   ];
+  
+//   return (
+//     <div className="todo-app">
+//       <AppHeader toDo={1} done={3} />
+//       <div className="top-panel d-flex">
+//         <SearchPanel />
+//         <ItemStatusFilter />
+//       </div>
+
+//       <TodoList todos={todoData}
+//       onDeleted={ (id) => console.log('del', id)} />
+//     </div>
+//   );
+// };
+
